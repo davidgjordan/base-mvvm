486a1a16d5f08ea3f3fd07554a620368104841ce
David Granado J <david.granado@interfase.com.bo>
2022-06-30 11:22:37 -0400

    Update project name

---
+++ base-mvvm/modules/auth/signup/controllers/SignupViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignupViewController.swift
+//  social_network
+//
+//  Created by David Granado Jordan on 6/28/22.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    let firebaseManager = FirebaseManager.shared
+
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Sign Up"
+    }
+    
+    @IBAction func addUser(_ sender: Any) {
+        let name = nameTextField.text ?? ""
+        let age = Int(ageTextField.text ?? "18") ?? 18
+        let email = emailTextField.text ?? "test@user.com"
+        let pass = passwordTextField.text ?? "pass"
+        
+        SignUpViewModel.shared.addUser(name: name, age: age, email: email, password: pass) { user in
+            if user != nil {
+                self.navigationController?.popViewController(animated: true)
+            } else {
+                print("Error to create a user")
+            }
+        }
+    }
+
+}
---
+++ base-mvvm/repository/network/managers/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  social_network
+//
+//  Created by David Granado Jordan on 6/29/22.
+//
+
+import Foundation
